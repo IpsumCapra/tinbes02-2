55b740df00d107de274a4026e272a9c1390a85f4
IpsumCapra <samuelcornelisse@gmail.com>
2021-05-27 11:12:58 +0200

    Added new filesystem function definitions.

--- src/filesystem.h
+++ src/filesystem.h
@@ -4,9 +4,6 @@
 #ifndef ARDUINOS_FILESYSTEM_H
 #define ARDUINOS_FILESYSTEM_H
 
-EERef noOfFiles = EEPROM[0];
-int diskSize;
-
 struct FATEntry {
     char filename[NAMESIZE];
     int start;
@@ -21,6 +18,12 @@ bool deleteFATEntry(int file);
 
 int lookupEntry(char name[NAMESIZE]);
 
-void defrag();
+bool storeFile(char name[NAMESIZE], int size);
+bool retrieveFile(char name[NAMESIZE]);
+bool deleteFile(char name[NAMESIZE]);
+
+int freeSpace();
+
+void listFiles();
 
 #endif //ARDUINOS_FILESYSTEM_H
